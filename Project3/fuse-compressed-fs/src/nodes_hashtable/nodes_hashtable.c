6ef53e62868bc1fd039638fcd7f38e0d329bb494
Jason Nikas <nikasjason1@gmail.com>
2025-06-09 16:37:52 +0300

    Fix bugs related to the node hashtable

--- Project3/fuse-compressed-fs/src/nodes_hashtable/nodes_hashtable.c
+++ Project3/fuse-compressed-fs/src/nodes_hashtable/nodes_hashtable.c
@@ -5,11 +5,11 @@
 
 unsigned long last_id = -1;
 
-nodes_hash_element_t *nodes_table_add_new(nodes_hash_element_t **table, const char name[NAME_MAX]) {
-  	return nodes_table_add(table, name, ++last_id);
+nodes_hash_element_t *nodes_table_add_new(nodes_hash_element_t **table, const char name[NAME_MAX + 1], bool is_dir) {
+  	return nodes_table_add(table, name, is_dir, ++last_id);
 }
 
-nodes_hash_element_t *nodes_table_add(nodes_hash_element_t **table, const char name[NAME_MAX], unsigned long id) {
+nodes_hash_element_t *nodes_table_add(nodes_hash_element_t **table, const char name[NAME_MAX + 1], bool is_dir, unsigned long id) {
 	nodes_hash_element_t *element;
 
 	HASH_FIND_PTR(*table, name, element);
@@ -17,15 +17,16 @@ nodes_hash_element_t *nodes_table_add(nodes_hash_element_t **table, const char n
 		return NULL;
 
 	element = malloc(sizeof *element);
-
 	element->id = id;
 	strncpy(element->name, name, NAME_MAX);
+	element->is_dir = is_dir;
 	element->hashmap = NULL;
+	HASH_ADD_PTR(*table, name, element);
 
 	return element;
 }
 
-int nodes_table_remove(nodes_hash_element_t **table, const char name[NAME_MAX]) {
+int nodes_table_remove(nodes_hash_element_t **table, const char name[NAME_MAX + 1]) {
 	nodes_hash_element_t *element;
 
 	HASH_FIND_PTR(*table, name, element);
@@ -38,12 +39,45 @@ int nodes_table_remove(nodes_hash_element_t **table, const char name[NAME_MAX]) 
 	return 0;
 }
 
-nodes_hash_element_t *nodes_table_find(nodes_hash_element_t *table, const char name[NAME_MAX]) {
+nodes_hash_element_t *nodes_table_find(nodes_hash_element_t *table, const char name[NAME_MAX + 1]) {
     nodes_hash_element_t *element;
 
 	HASH_FIND_PTR(table, name, element);
 
     return element;
+}
+
+void print_dir(nodes_hash_element_t *element, int depth) {
+	if (element == NULL)
+		return;
+
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("|-");
+	log_msg("name: %s\n", element->name);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("element: %p\n", element);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("hashmap: %p\n", element->hashmap);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("id: %lu\n", element->id);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("is_dir: %d\n", element->is_dir);
+	if (element->is_dir)
+		print_dir(element->hashmap, depth + 2);
+}
+
+void nodes_table_print(nodes_hash_element_t *table) {
+	nodes_hash_element_t *element;
+
+	log_msg("###########################################################\n");
+	for (element = table; element != NULL; element = element->hh.next) {
+		print_dir(element, 2);
+	}
+	log_msg("###########################################################\n");
 }
 
 void nodes_table_clear_foreach(nodes_hash_element_t *table, void (*func)(nodes_hash_element_t *, int), int fd) {
