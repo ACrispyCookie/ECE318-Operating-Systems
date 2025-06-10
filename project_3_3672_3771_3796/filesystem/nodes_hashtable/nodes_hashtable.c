f5f2f9311e3452efd87e05627ad0bd82a8352bfa
TsiantosD <tsiantos.d@gmail.com>
2025-06-10 16:56:18 +0300

    Write custom Makefile, add folder structure required for grading

---
+++ project_3_3672_3771_3796/filesystem/nodes_hashtable/nodes_hashtable.c
@@ -0,0 +1,129 @@
+#include "nodes_hashtable.h"
+#include "log.h"
+#include <limits.h>
+#include <stdio.h>
+
+unsigned long last_id = 0;
+
+nodes_hash_element_t *nodes_table_add_new(nodes_hash_element_t **table, const char name[NAME_MAX + 1], bool is_dir) {
+  	return nodes_table_add(table, name, is_dir, last_id++);
+}
+
+nodes_hash_element_t *nodes_table_add(nodes_hash_element_t **table, const char name[NAME_MAX + 1], bool is_dir, unsigned long id) {
+	nodes_hash_element_t *element;
+
+	HASH_FIND_STR(*table, name, element);
+	if (element != NULL)
+		return NULL;
+
+	element = malloc(sizeof *element);
+	element->id = id;
+	strncpy(element->name, name, NAME_MAX);
+	element->is_dir = is_dir;
+	element->hashmap = NULL;
+	HASH_ADD_STR(*table, name, element);
+
+	return element;
+}
+
+int nodes_table_remove(nodes_hash_element_t **table, const char name[NAME_MAX + 1]) {
+	nodes_hash_element_t *element;
+
+	HASH_FIND_STR(*table, name, element);
+	if (element == NULL)
+		return 1;
+
+	HASH_DEL(*table, element);
+	free(element);
+
+	return 0;
+}
+
+int nodes_table_add_element(nodes_hash_element_t **table, nodes_hash_element_t *element) {
+	nodes_hash_element_t *found;
+	HASH_FIND_STR(*table, element->name, found);
+	if (found != NULL)
+		return 1;
+
+	HASH_ADD_STR(*table, name, element);
+	return 0;
+}
+
+int nodes_table_remove_element(nodes_hash_element_t **table, nodes_hash_element_t *element) {
+	nodes_hash_element_t *found;
+
+	HASH_FIND_STR(*table, element->name, found);
+	if (found == NULL)
+		return 1;
+	
+	HASH_DEL(*table, found);
+	return 0;
+}
+
+nodes_hash_element_t *nodes_table_find(nodes_hash_element_t *table, const char name[NAME_MAX + 1]) {
+    nodes_hash_element_t *element;
+
+	HASH_FIND_STR(table, name, element);
+
+    return element;
+}
+
+void print_dir(nodes_hash_element_t *element, int depth) {
+	if (element == NULL)
+		return;
+
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("|-");
+	log_msg("name: %s\n", element->name);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("element: %p\n", element);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("hashmap: %p\n", element->hashmap);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("id: %lu\n", element->id);
+	for (int i = 0; i < depth - 2; i++) log_msg(" ");
+	log_msg("| ");
+	log_msg("is_dir: %d\n", element->is_dir);
+	if (element->is_dir) {
+		for (nodes_hash_element_t *dir_element = element->hashmap; dir_element != NULL; dir_element = dir_element->hh.next) {
+			print_dir(dir_element, depth + 2);
+		}
+	}
+}
+
+void nodes_table_print(nodes_hash_element_t *table) {
+	nodes_hash_element_t *element;
+
+	log_msg("###########################################################\n");
+	for (element = table; element != NULL; element = element->hh.next) {
+		print_dir(element, 2);
+	}
+	log_msg("###########################################################\n");
+}
+
+void nodes_table_clear_foreach(nodes_hash_element_t *table, void (*func)(nodes_hash_element_t *, int), int fd) {
+	nodes_hash_element_t *curr, *tmp;
+
+	HASH_ITER(hh, table, curr, tmp) {
+		if (func != NULL)
+			func(curr, fd);
+
+		HASH_DEL(table, curr);
+		free(curr);
+	}
+}
+
+void nodes_table_clear(nodes_hash_element_t *table) {
+	nodes_table_clear_foreach(table, NULL, -1);
+}
+
+void set_last_id(unsigned long id) {
+	last_id = id;
+}
+
+unsigned long get_last_id() {
+	return last_id;
+}
